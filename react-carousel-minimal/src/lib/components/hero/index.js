54cc13af85ed7fa087ba9034cf6d8ac36bf81e69
josuel <105312703+josuelmroczko@users.noreply.github.com>
2023-01-17 17:00:55 -0300

    novo

--- react-carousel-minimal/src/lib/components/hero/index.js
+++ react-carousel-minimal/src/lib/components/hero/index.js
@@ -6,9 +6,14 @@ function Hero() {
             <div className="hero">
                 <section className="section__empresa">
                     <section className="section__quem">
-                        <h1 id="projeto__nome" className="titulo__empresa">QUEM SOMOS</h1>
+                        <h1 id="projeto__nome" className="titulo__empresa">Quem somos?</h1>
                         <p className="paragrafo paragrafo__empresa">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel est tortor. Ut eget nulla vel lorem convallis semper et eu ante.
+                        Somos uma organização não-governamental com sede em Duque de Caxias-RJ.
+Atualmente a instituição é formada por uma equipe de mais de 50 voluntários, das
+áreas de RH, Pedagogia, Publicidade e Marketing e Tecnologia.
+Temos o programa Trilha de Carreira, que visa o desenvolvimento pessoal e
+profissional através do nosso ambiente de voluntariado, funcionando como um
+laboratório de experiências profissionais.
 
                         </p>
                     </section>
